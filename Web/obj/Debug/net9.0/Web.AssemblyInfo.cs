0da46d43612972235351541e219f313eeece18ef
July <danielitha345ra@gmail.com>
2025-05-16 17:13:25 -0500

    cambiso

--- Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2350addf3b94a1855533c68ca2fef59db345ea92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19eabbbff06b9c40ee827ae5291cd2597c2f258")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
