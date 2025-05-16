e35c1b566d0a8467f21412227c930218ae145db4
bscl <brayanstidcorteslombana@gmail.com>
2025-05-16 13:48:06 -0500

    carpetas

--- Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176ffbc9099f6fa3907a7408d41f98a54050a388")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72cf60d61681ef681ed24314ba8ca014191a3c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
