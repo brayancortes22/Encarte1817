2350addf3b94a1855533c68ca2fef59db345ea92
DIEGO-ALVARADO07 <daalvarado2007@gmail.com>
2025-05-16 16:43:32 -0500

    cambiso

---
+++ Data/Interfaces/IUserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity.Model;
+
+namespace Data.Interfaces
+{
+    public interface IUserData
+    {
+        Task<User> LoginAsync(string email, string password);
+        Task<User> ChangePasswordAsync(int userId,string password);
+        Task<User> SendEmailAsync(string email);
+    }
+}
