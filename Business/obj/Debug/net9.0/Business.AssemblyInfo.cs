4fa849b9b8a3cb9489675a32348f8a1b2f97f76e
July <danielitha345ra@gmail.com>
2025-05-17 03:05:54 -0500

    cambiso

--- Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deef8f1822d7dc006c6f750de4015506ceb7b047")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e0bf92152217ebdd3527178de811d70a2f0db16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
