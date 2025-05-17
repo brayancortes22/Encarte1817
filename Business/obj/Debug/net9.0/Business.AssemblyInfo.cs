d6b1bf1fc8f5b7bdfb3179654d1ea8d9167c8bf9
Daniel <Daniel3204155185@gmail.com>
2025-05-17 03:45:15 -0500

    ahi vamos

--- Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9575b12a5a47e1fa16d3cedca8b8f0d96c05d8fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb3de0f9b884167bb423db03645e7c6592b46d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
